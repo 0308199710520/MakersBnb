84f16013c4e47e5345027e79e03e7045b4ed84ac
ltaylorbrown <lauren2.brown.lb@googlemail.com>
2021-08-19 10:33:41 +0100

    listing tables added - all tests pass - it's awesome

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -4,15 +4,16 @@ feature 'Homepage test' do
     expect(page).to have_content ('Feel at home, anywhere')
   end 
 
-  scenario 'inputs email address, password and password confirmation' do 
-    visit('/')
-    fill_in('email', with: 'user@gmail.com') 
-    fill_in('password', with: 'password')
-    fill_in('confirm_password', with: 'password')
-    click_button('Submit')
-
-    expect(page).to have_content('Login to MakersBnB')
-  end 
+  feature
+    scenario 'inputs email address, password and password confirmation' do 
+      visit('/')
+      fill_in('email', with: 'user@gmail.com') 
+      fill_in('password', with: 'password')
+      fill_in('confirm_password', with: 'password')
+      click_button('Submit')
+    
+      expect(page).to have_content('Login to MakersBnB')
+    end 
 
   scenario 'homepage has a sign in button' do
     visit('/')
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,6 +1,4 @@
 feature 'login' do 
-
-
   scenario 'a user can login with correct password' do 
     visit '/login'
     connection = PG.connect(dbname: 'makersbnb_test')
@@ -12,14 +10,21 @@ feature 'login' do
     expect(page.current_path).to eq ('/listings')
   end
 
-  scenario 'a user can login with correct password' do 
+  scenario 'a user cannot login with incorrect password' do 
     visit '/login'
     connection = PG.connect(dbname: 'makersbnb_test')
     connection.exec("INSERT INTO user_info (email, password) VALUES ('user@gmail.com', 'password123');")
     fill_in("email", with: 'user@gmail.com') 
     fill_in('password', with: 'wrongpassword')
     click_button('Login')
-
+    
     expect(page.current_path).to eq ('/login')
+  end
+end 
+feature 'button to sign up page' do 
+  scenario 'login page has a link to go back to the signup page' do 
+    visit ('/login')
+    click_link ('Not a member? Sign up here!')
+    expect(current_path).to eq '/'
   end
 end 
