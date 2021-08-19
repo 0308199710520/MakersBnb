005635cbac50732b36d92fe7b6da34f9355dd1fc
Michael Poole <53351111+0308199710520@users.noreply.github.com>
2021-08-19 11:44:39 +0100

    Update user.rb

--- lib/user.rb
+++ lib/user.rb
@@ -1,29 +1,3 @@
-# require 'pg'
-# ​
-# class User
-# ​
-#   def create(email:, password:)
-#       connection = connection()
-#       result = connection.exec("INSERT INTO user_info (email, password) VALUES ('#{email}', '#{password}');")
-# ​  end
-# ​
-#   def login(email:, password:)
-# ​    connection = connection()
-# ​    result = connection.exec("SELECT * FROM user_info WHERE email LIKE '#{email}' AND password LIKE '#{password}';")
-#     result = result.map { |email| email['email'] }
-#     return result.length > 0 
-#   end 
-# ​
-#   def connection
-#     if ENV['ENVIRONMENT'] == 'test'
-#       connection = PG.connect(dbname: 'makersbnb_test')
-#     else 
-#       connection = PG.connect(dbname: 'makersbnb')
-#     end
-#   end
-
-# end
-
 
 require 'pg'
 
@@ -46,12 +20,7 @@ class User
   end 
 
   def password_same?(password:, confirm_password:)
-    if password == confirm_password
-      return true
-    else
-      return false
-    end
-  end 
+    return password == confirm_password
 
   def connection
     if ENV['ENVIRONMENT'] == 'test'
